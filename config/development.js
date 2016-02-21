6dc411befbd5f6bc94c62929c49d2bf1d76d4f64
Oleksiy Brain <arro.social@gmail.com>
2016-02-21 16:04:58 +0200

    Untrack config file

--- config/development.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    port: process.env.PORT || 3001,
-    gateway: process.env.GATEWAY || 'localhost:3003',
-    ssl: false // alternatively - ssl: { key: 'path to ssl private key', cert: 'path to ssl certificate' }
-};
